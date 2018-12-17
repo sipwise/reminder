5953bc6862fafaa835d285ea3c940e8fd2f9d035
Guillem Jover <gjover@sipwise.com>
2018-12-17 14:14:46 +0100

    TT#49150 Namespace program names with ngcp-
    This makes it obvious these are NGCP specific programs, and avoids
    possible collisions with other third party programs.
    
    Create backward compatibility symlinks for a smoother transition.
    
    Namespace too all debhelper fragment files.
    
    Change-Id: Iea96d305e8fcd2461cdb4c8d28bef5d55b307ffe

---
+++ debian/ngcp-reminder.cron.d
@@ -0,0 +1,1 @@
+* * * * * root if /usr/sbin/ngcp-check-active -q; then /usr/sbin/ngcp-reminder 1>/dev/null; fi
--- debian/reminder.cron.d
+++
@@ -1,1 +0,0 @@
-* * * * * root if /usr/sbin/ngcp-check-active -q; then /usr/sbin/reminder 1>/dev/null; fi
